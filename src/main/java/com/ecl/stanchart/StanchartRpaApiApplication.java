3aa517f2446dea7890bc08d413b21d1c17aa29bc
E.OFORI <e.ofori@entcomputing.com>
2022-02-16 19:34:37 +0000

    updated JDBC Driver and changed port to 8081

--- src/main/java/com/ecl/stanchart/StanchartRpaApiApplication.java
+++ src/main/java/com/ecl/stanchart/StanchartRpaApiApplication.java
@@ -2,11 +2,21 @@ package com.ecl.stanchart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@ComponentScan(basePackages = "com.ecl")
 @SpringBootApplication
 public class StanchartRpaApiApplication {
+
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StanchartRpaApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StanchartRpaApiApplication.class, args);
@@ -15,6 +25,19 @@ public class StanchartRpaApiApplication {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/").allowedOrigins("*");
+			}
+
+		};
+
 	}
 
 
